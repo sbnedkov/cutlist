88587ebc97d5890e9891eb9c6f12379ffb379b8d
Svetoslav Nedkov <sveto.n@gmail.com>
2016-02-01 23:03:59 +0200

    fix imports for SolverBnB; Remove unneeded parseInt on body parameters

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import I18n from 'i18n-2';
-import {SolverBnB} from 'guillotine-solver';
+import SolverBnB from 'guillotine-solver';
 
 import middleware from './src/middleware';
 
@@ -41,16 +41,17 @@ app.post('/cutlist', (req, res) => {
     // TODO: many stock sheets
     parts.forEach(item => {
         if (item.ref) { // TODO: better way to filter unwanted items
-            itemsw.push(parseInt(item.w));
-            itemsh.push(parseInt(item.h));
-            demands.push(parseInt(item.q));
+            itemsw.push(item.w);
+            itemsh.push(item.h);
+            demands.push(item.q);
             // item.ref
             // item.canRotate
         }
     });
 
-    var solver = new SolverBnB([parseInt(stocks[0].w)], [parseInt(stocks[0].h)], itemsw, itemsh, demands);
+    var solver = new SolverBnB([stocks[0].w], [stocks[0].h], itemsw, itemsh, demands);
     var result = solver.solve();
+
     console.log(result);
 
     res.json(result);
