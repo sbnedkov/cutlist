c97ca8019424a70f4956bdfd19520838b9b37d16
Svetoslav Nedkov <sveto.n@gmail.com>
2018-06-05 22:48:50 +0300

    Fix bitbucket address

--- setup-ssh.sh
+++ setup-ssh.sh
@@ -18,7 +18,7 @@ if [ "$GIT_SSH_KEY" != "" ]; then
 
   # Setup the ssh config file.
   # Switch out the hostname for different hosts.
-  echo -e "Host bitbucket.com\n"\
+  echo -e "Host bitbucket.org\n"\
           " IdentityFile ~/.ssh/deploy_key\n"\
           " IdentitiesOnly yes\n"\
           " UserKnownHostsFile=/dev/null\n"\
