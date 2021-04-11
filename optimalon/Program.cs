774d626af8dffef3c6164a0f0f8c663dcb22f617
Svetoslav Nedkov <sveto.n@gmail.com>
2021-04-11 16:32:33 +0300

    Correctly output new lines

--- optimalon/Program.cs
+++ optimalon/Program.cs
@@ -54,8 +54,8 @@ namespace optimalon
           double width,height,X,Y,W,H;
           bool rotated,stockActive;
           string Txt;
-          Console.Write("Used {0} stocks\n", Calculator.UsedStockCount);
-          Console.Write("Created {0} different layouts\n", Calculator.LayoutCount);
+          Console.WriteLine("Used {0} stocks", Calculator.UsedStockCount);
+          Console.WriteLine("Created {0} different layouts", Calculator.LayoutCount);
           // Iterate by each layout and output information about each layout,
           // such as number and length of used stocks and part indices cut from the stocks
           for (iLayout = 0; iLayout < Calculator.LayoutCount; iLayout++) {
@@ -63,7 +63,7 @@ namespace optimalon
             // Output information about each stock, such as stock Length
             for (iStock = stockIndex; iStock < stockIndex + stockCount; iStock++) {
               Calculator.GetStockInfo(iStock, out width, out height, out stockActive);
-              Console.Write("Stock={0}: Width={1}; Height={2}\n", iStock, width, height);
+              Console.WriteLine("Stock={0}: Width={1}; Height={2}", iStock, width, height);
               // Output the information about parts cut from this stock
               // First we get quantity of parts cut from the stock
               partCount = Calculator.GetPartCountOnStock(iStock);
@@ -76,12 +76,12 @@ namespace optimalon
                 // W,H – widht and height of the part partIndex
                 // X,Y – coordinates of the top left corner of the part on the stock iStock
                 // If rotated is true then the part has been roated by 90°
-                Console.Write("Part={0}; stock={1}; Width={2}; Height={3}; X={4}; Y={5}; R={6}\n", partIndex, iStock, W, H, X, Y, rotated);
+                Console.WriteLine("Part={0}; stock={1}; Width={2}; Height={3}; X={4}; Y={5}; R={6}", partIndex, iStock, W, H, X, Y, rotated);
               }
             }
           }
         } else {
-          Console.Write("%S", result);
+          Console.WriteLine("%S", result);
           return;
         }
       } catch (Exception e) {
