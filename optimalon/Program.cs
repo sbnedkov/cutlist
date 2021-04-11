a06c7afed4244ff254dbccc3ba000357762926e4
Svetoslav Nedkov <sveto.n@gmail.com>
2021-04-11 15:28:56 +0300

    Add missing output

--- optimalon/Program.cs
+++ optimalon/Program.cs
@@ -48,7 +48,39 @@ namespace optimalon
         }
 
         string result = Calculator.Execute();
-        if (result != "") {
+
+        if (result == "") {
+          int stockIndex,stockCount,iPart,iLayout,partCount,partIndex,tmp,iStock;
+          double width,height,X,Y,W,H;
+          bool rotated,stockActive;
+          string Txt;
+          Console.Write("Used {0} stocks", Calculator.UsedStockCount);
+          Console.Write("Created {0} different layouts", Calculator.LayoutCount);
+          // Iterate by each layout and output information about each layout,
+          // such as number and length of used stocks and part indices cut from the stocks
+          for (iLayout = 0; iLayout < Calculator.LayoutCount; iLayout++) {
+            Calculator.GetLayoutInfo(iLayout, out stockIndex, out stockCount);
+            // Output information about each stock, such as stock Length
+            for (iStock = stockIndex; iStock < stockIndex + stockCount; iStock++) {
+              Calculator.GetStockInfo(iStock, out width, out height, out stockActive);
+              Console.Write("Stock={0}:  Width={1}; Height={2}", iStock, width, height);
+              // Output the information about parts cut from this stock
+              // First we get quantity of parts cut from the stock
+              partCount = Calculator.GetPartCountOnStock(iStock);
+              // Iterate by parts and get indices of cut parts
+              for (iPart = 0; iPart < partCount; iPart++) {
+                // Get global part index of iPart cut from the current stock
+                partIndex = Calculator.GetPartIndexOnStock(iStock, iPart);
+                // Get sizes and location of the source part with index partIndex
+                Calculator.GetResultPart(partIndex, out tmp, out W, out H, out X, out Y, out rotated);
+                // W,H – widht and height of the part partIndex
+                // X,Y – coordinates of the top left corner of the part on the stock iStock
+                // If rotated is true then the part has been roated by 90°
+                Console.Write("Part={0}; stock={1}; Width={2}; Height={3}; X={4}; Y={5}", partIndex, iStock, W, H, X, Y);
+              }
+            }
+          }
+        } else {
           Console.Write("%S", result);
           return;
         }
